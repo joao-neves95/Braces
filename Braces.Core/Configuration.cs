ae9748cfa6e212c40d0a4b523003baa79f34e3f4
shivayl <joao95neves@gmail.com>
2018-07-02 00:31:13 +0100

    Added the Configuration class

---
+++ Braces.Core/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Braces.Core.Models;
+
+namespace Braces.Core
+{
+    class Configuration
+    {
+        private ConfigurationModel GetDefaultConfiguration()
+        {
+            ConfigurationModel defaultConfiguration = new ConfigurationModel() {
+                Modifier = "Control",
+                SaveKey = "S"
+            };
+
+            return defaultConfiguration; 
+        }
+    }
+}
