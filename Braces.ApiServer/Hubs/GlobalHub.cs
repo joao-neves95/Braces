7c9e36fc96c55c1b75bab301e251bbf300a76869
João Neves <joao95neves@gmail.com>
2019-04-27 02:13:32 +0100

    Update { BRACES }
    - Added and prepared the Braces.ApiServer SignalR server;
    - Small fix on the TestPlugin onOpen event handler.

---
+++ Braces.ApiServer/Hubs/GlobalHub.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using System.Windows;
+using Braces.Core.ExtensionSystem;
+using Braces.Core.Enums;
+
+namespace Braces.ApiServer.Hubs
+{
+    public class GlobalHub : Hub<Plugin>
+    {
+        #region PROPERTIES
+
+        private readonly ExtensionSystem ExtSystem = ExtensionSystem.Instance;
+
+        #endregion
+
+        #region METHODS
+
+        public async Task FireEvent( string eventName, string fileTypeName, object sender, RoutedEventArgs e, object args )
+        {
+            if (!ExtSystem.PluginManager.PluginIdsByFileType.ContainsKey( fileTypeName ))
+                return;
+
+            Plugin thisGroup = Clients.Group( fileTypeName );
+            Plugin currentPlugin;
+
+            for (int i = 0; i < ExtSystem.PluginManager.PluginIdsByFileType[fileTypeName].Count; ++i)
+            {
+                currentPlugin = ExtSystem.PluginManager.Plugins[ExtSystem.PluginManager.PluginIdsByFileType[fileTypeName][i]];
+
+                if (!currentPlugin.ImplementsMethod( eventName ))
+                    continue;
+
+                switch (eventName)
+                {
+                    case EventName.OnFileSave:
+                        await thisGroup.OnFileSave( sender, e, args );
+                        break;
+                    case EventName.OnFileOpen:
+                        await thisGroup.OnFileOpen( sender, e, args );
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public async Task AddEvent()
+        {
+            return;
+        }
+
+        #endregion
+    }
+}
