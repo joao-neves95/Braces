29ffb0c17ed2be2e2ad9af98c44e9b1942cfda43
João Neves <joao95neves@gmail.com>
2019-07-30 23:22:25 +0100

    Added Braces Cursed (curses UI version of BRACES).

---
+++ Braces.UI.Curses/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Terminal.Gui;
+
+namespace Braces.UI.Curses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.Init();
+            new MainWindow();
+            Application.Run();
+        }
+    }
+}
